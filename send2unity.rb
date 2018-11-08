a7ab70dc6270a63e06622da9db339fcf8eb95d20
hsm-hx <skytea922@gmail.com>
2018-11-09 01:25:06 +0900

    :heayv_plus_sign: 引数で連鎖キーワードを受け取れるようにした

--- send2unity.rb
+++ send2unity.rb
@@ -2,6 +2,12 @@ require './markov.rb'
 require 'socket'
 
 def main()
+  if (ARGV[0] and ARGV[1]) != nil 
+    keyword = [ARGV[0], ARGV[1].to_i]
+  else
+    keyword = -1
+  end
+
   # 会話文を生成
   sentence = generate_text_from_json(keyword, "")
 
